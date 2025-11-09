1995ebc15862a23539d3f9f641f41c51e494867d
Ezedi <ezedinawol130@gmail.com>
2025-11-09 15:21:21 +0300

    feat: register page UI done

---
+++ lib/pages/auth/register_page.dart
@@ -0,0 +1,116 @@
+import 'package:e_commerce/pages/auth/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../component/reusable_textfield.dart';
+
+class RegisterPage extends StatelessWidget {
+   RegisterPage({super.key});
+   final firstnameController = TextEditingController();
+   final lastnameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 16),
+          Text(
+            'WELCOME',
+            style: GoogleFonts.lato(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            'Login Now!',
+            style: GoogleFonts.lato(
+                fontSize: 15.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+            ),
+          ),
+          const SizedBox(
+            height: 25.0,
+          ),
+          ReUsableTextField(
+            controller: firstnameController,
+            text: "first name",
+            obscure: false,
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          ReUsableTextField(
+            controller: lastnameController,
+            text: "last name",
+            obscure: false,
+          ),
+          const SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: SizedBox(
+              width: double.infinity,
+              height: 45,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF4EB3E8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  elevation: 3,
+                ),
+                onPressed: (){},
+                child: Text('Register',style: GoogleFonts.lato(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                ),),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Already a member?',
+                style: GoogleFonts.lato(
+                  color: Colors.grey[700],
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 5),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Login now',
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
