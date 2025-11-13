54c66217cf9e19b3bd983f806914c74bf871fb43
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-11-13 14:59:51 +0300

    feat: handles user registration

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,16 +1,32 @@
 import 'package:e_commerce/core/api_client.dart';
+import 'package:e_commerce/model/register_response.dart';
 import '../model/auth_response_dto.dart';
+import '../model/user_register_dto.dart';
 import '../model/user_login_dto.dart';
 
 class AuthService {
   Future<AuthResponseDto?> login(UserLoginDto dto) async {
     try {
-      final response = await ApiClient.post('/user/login', dto.toJsonString());
+      final response = await ApiClient.login('/user/login', dto.toJsonString());
 
       if (response.statusCode == 200) {
         return AuthResponseDto.fromJsonString(response.body);
       } else {
         print('Login failed: ${response.body}');
+        return null;
+      }
+    } catch (e) {
+      print('Error: $e');
+      return null;
+    }
+  }
+  Future<RegisterResponse?> register(UserRegisterDto dto) async {
+    try {
+      final response = await ApiClient.register('/user/create-user', dto.toJsonString());
+      if (response.statusCode == 200) {
+        return RegisterResponse.fromJsonString(response.body);
+      } else {
+        print('Registration failed: ${response.body}');
         return null;
       }
     } catch (e) {
