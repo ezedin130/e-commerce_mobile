f96137619ed6ebd7276ae5a5a7b537420f274500
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-11-13 12:09:38 +0300

    feat: Add ApiClient class for handling HTTP POST requests

---
+++ lib/core/api_client.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+class ApiClient {
+  static const String baseUrl = 'http://10.172.37.21:8080';
+
+  static Map<String, String> headers = {
+    'Content-Type': 'application/json',
+  };
+
+  static Uri uri(String endpoint) {
+    return Uri.parse('$baseUrl$endpoint');
+  }
+
+  static Future<http.Response> post(String endpoint, String body) async {
+    return await http.post(uri(endpoint), headers: headers, body: body);
+  }
+}
