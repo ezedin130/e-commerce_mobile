9cd71f59daefffeb459d1b7a3e7172e156145e20
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-11-14 21:14:06 +0300

    feat: home page UI made that includes logo , search field and category prototype

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,14 +6,116 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: GoogleFonts.lato(
-          color: Colors.white,
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'ShopHub',
+                    style: GoogleFonts.lato(
+                      color: Colors.blue,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                    ),
+                    Icon(Icons.shopping_cart)
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search Products , Shops...',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0)
+                    )
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                'Categories',
+                style: GoogleFonts.lato(
+                  color: Colors.black,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 5.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CategoryContainer(
+                          icon: Icons.home,
+                          text: 'Home',
+                        ),
+                        CategoryContainer(
+                          icon: Icons.computer,
+                          text: 'Computer',
+                        ),
+                        CategoryContainer(
+                          icon: Icons.phone,
+                          text: 'Phone',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CategoryContainer extends StatelessWidget {
+  const CategoryContainer({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 80.0,
+      height: 70.0,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon),
+          const SizedBox(height: 5.0,),
+          Text(text)
+        ],
       ),
     );
   }
